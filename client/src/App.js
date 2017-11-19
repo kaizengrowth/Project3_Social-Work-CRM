a7bb83105be2ab37dd4976da0783af05d35e79ff
Kai Lin Zhang <kaizen.agility@gmail.com>
2017-11-18 19:38:25 -0500

    Transformed into components: MainNav, Banner, AnimationHeader. Created App.css and edited broken syntax to jsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 import TopNav from './components/TopNav'
+import MainNav from './components/MainNav'
+import Banner from './components/Banner'
+import AnimationHeader from './components/AnimationHeader'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <TopNav />
-
+        <MainNav />
+        <Banner />
+        <AnimationHeader />
       </div>
     );
   }
 }
 
-export default App;
+export default App
