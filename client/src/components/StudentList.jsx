d36c7f9b3db5c1d7afb6b93d1f4662ff416d065d
bart369 <bartinpeguero@gmail.com>
2017-11-19 14:48:18 -0500

    StudentList renders the list of students. Li's will also have a button

--- client/src/components/StudentList.jsx
+++ client/src/components/StudentList.jsx
@@ -2,7 +2,32 @@ import React from 'react';
 
 class StudentList extends React.Component{
   constructor(props){
-    super();
+    super(props);
+    this.state = {
+      isPriority: null,
+      isResolved: null,
+      inProgress: null,
+    }
+  }
+
+  render(){
+    return (
+      <div className = 'studentList'>
+        {this.props.studentDataLoaded ?
+        <ul className = 'isPriority'>
+          {this.props.studentList.map(student
+          return (
+            {/* Map should return a li item for each student. The key of each li will be the student's id and
+            each li will also have a button. When the button is clicked it will invoke the currentprofile method (found on profile controller)
+            and pass that student's id as an argument*/}
+            <li key = {student.studid}><button className = 'button'
+            onClick = {() => this.props.currentProfile(student.studid)}>
+            {student.first_name +  " " + student.last_name}</button></li>
+          )
+        )}
+        </ul>
+        : (<h2> Still Loading Student Data </h2> )}
+    )
   }
 
 
