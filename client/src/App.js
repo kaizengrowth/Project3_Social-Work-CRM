3732cd9d55e6e67a406de4598a4d995b03b5e657
Kai Zhang <kaizen.agility@gmail.com>
2017-11-20 15:45:35 -0500

    Began adding React login routes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,82 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import './App.css'
 import TopNav from './components/TopNav'
 import MainNav from './components/MainNav'
 import Banner from './components/Banner'
 import AnimationHeader from './components/AnimationHeader'
+import Home from './containers/Home'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <TopNav />
-        <MainNav />
-        <Banner />
-        <AnimationHeader />
-      </div>
-    );
-  }
-}
+    constructor() {
+        super();
+        this.state = {
+            auth: false,
+            student: null,
+        }
+        // this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+        // this.handleRegisterSubmit = this.handleRegisterSubmit.bind(this);
+        // this.logout = this.logout.bind(this);
+    }
+
+    // componentDidMount() {
+    //     fetch('/api/auth/verify', { credentials: 'include'})
+    //     .then(res => res.json())
+    //     .then(res => {
+    //         this.setState({
+    //             auth: res.auth,
+    //             student: res.data.student,
+    //         })
+    //     }).catch(err => console.log(err));
+    // }
+
+    // handleLoginSubmit(e, data) {
+    //     e.preventDefault();
+    //     fetch('/api/auth/login', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         credentials: 'include',
+    //         body: JSON.stringify(data),
+    //     }).then(res => res.json())
+    //     .then(res => {
+    //         console.log(res);
+    //         this.setState({
+    //             auth: res.auth,
+    //             student: res.data.student,
+    //         }).catch(err => console.log(err));
+    //     })
+
+    // handleRegisterSubmit(e, data) {
+    //     e.preventDefault();
+    //     console.log(data);
+    //     fetch('/api/auth/register', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify(data),
+    //     }).then(res => res.json())
+    //     .then(res => {
+    //         this.setState({
+    //             auth: res.auth,
+    //         }).catch(err => console.log(err));
+    // }
+
+
+      render() {
+        return (
+            <Router>
+              <div className="App">
+                // <TopNav logout={this.logout} />
+                <MainNav />
+                <Banner />
+                <AnimationHeader />
+              </div>
+            </Router>
+        );
+      }
+    }
 
 export default App
--- server.js
+++ server.js
@@ -36,8 +36,8 @@ app.use(express.static('public'));
 
 // const authRoutes = require('./routes/auth-routes');
 // app.use('/api/auth', authRoutes);
-// const studentRoutes = require('./routes/student-routes.js');
-// app.use('api/students', studentRoutes);
+const studentRoutes = require('./routes/student-routes.js');
+app.use('api/students', studentRoutes);
 
 app.use('*', (req, res) => {
     res.status(404).send('Not Found');
