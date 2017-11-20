482946d5aeaed80c6a87d65326cf1a03dfffee38
bart369 <bartinpeguero@gmail.com>
2017-11-20 10:06:27 -0500

    worked on student profile

--- client/src/components/StudentProfile.jsx
+++ client/src/components/StudentProfile.jsx
@@ -4,15 +4,20 @@ const StudentProfile = (props) => {
 let student = props.selectedStudent;
   return (
     <div className = 'student_profile'>
-      <img src = {student.image} alt = '' />
+
+    {student ?
+
+      (<img src = {student.image} alt = '' />
       <div className = 'student_info'>
         <h1>{{student.first_name} + ' ' + {student.last_name}}</h1>
         <p>Email: {student.email}</p>
         <p>Phone number: {student.phone}</p>
         <p>{student.aboutme}</p>
       </div>
-      <h3>Student Notes: {student.studentnotes}</h3>
+      <h3>Student Notes: {student.studentnotes}</h3>)
 
+      : (<h1> No Student Selected </h1>)}
+      
     </div>
   )
 
