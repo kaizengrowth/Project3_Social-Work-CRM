25fde5f0a615b39ff1a085bfbcb7fa763502652e
andrea lee <lee.ays.lee@gmail.com>
2017-11-20 15:40:36 -0500

    this took way too long, but migrations and seed data for resources and case_conferences.  need to input remaining resources data but hoping to do that from .csv

--- db/migrations/migration-1120171030.sql
+++ db/migrations/migration-1120171030.sql
@@ -6,5 +6,19 @@ CREATE TABLE IF NOT EXISTS resources (
 );
 
 CREATE TABLE IF NOT EXISTS case_conferences (
-
+  casemtgid SERIAL PRIMARY KEY,
+  swid INT REFERENCES social_workers(swid),
+  studid INT REFERENCES students(studid),
+  todaysdate DATE,
+  strengths TEXT,
+  challenges TEXT,
+  challenge_steps TEXT,
+  help TEXT,
+  pershcolas_support TEXT,
+  interest_outside TEXT,
+  ifyes_what TEXT,
+  follow_ups TEXT,
+  fu_student TEXT,
+  fu_perscholas TEXT,
+  final_notes TEXT
 )
