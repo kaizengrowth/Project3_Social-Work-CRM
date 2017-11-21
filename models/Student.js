0133d4537f2eceef6da7a3392da3f7558c820327
andrea lee <lee.ays.lee@gmail.com>
2017-11-21 10:32:54 -0500

    registration - controller, model, routes done.  migration and seed files consolidated.  use most recent migration file and seed-update. sql  (class renamed to cycle)

--- models/Student.js
+++ models/Student.js
@@ -6,22 +6,41 @@ Student.findAll = () => {
     return db.manyOrNone('SELECT * FROM students');
   }
 
-Student.findbyUserName = userName => {
+//should this be email and not id?
+Student.findbyUserName = (email) => {
     return db.oneOrNone(`
-        SELECT * FROM users
-        WHERE username = $1
-    `, [userName]);
+        SELECT * FROM students
+        WHERE email = $1
+    `, [email]);
 };
 
 // TO DO - Update create model method below with password field when auth routes are done.
 
-Student.create = email => {
+//this was email i changed it to student
+Student.create = (student) => {
     return db.one(`
         INSERT INTO students
-        (first_name, last_name, phone, class, aboutme)
-        VALUES ($1, $2, $3, $4, $5, $6)
+        (first_name, last_name, phone, cycle, aboutme)
+        VALUES ($1, $2, $3, $4, $5)
         RETURNING *
-    `, [student.first_name, student.last_name, student.phone, student.class, student.aboutme]);
+    `, [student.email, student.first_name, student.last_name, student.phone, student.cycle, student.aboutme]);
+};
+
+Student.update = (student, id) => {
+  return db.one(`
+    UPDATE students SET
+    first_name = $1,
+    last_name = $2,
+    phone = $3,
+    cycle = $4,
+    aboutme = $5
+    WHERE id = $6
+    RETURNING *
+  `,[student.first_name, student.last_name, student.phone, student.cycle, student.aboutme, id);
+};
+
+Student.destroy = (id) => {
+  return db.none('DELETE FROM students WHERE id = $1', id);
 };
 
 module.exports = Student;
