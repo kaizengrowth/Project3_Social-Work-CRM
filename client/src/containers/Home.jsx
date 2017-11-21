2297bea7be223a7b78c480bc326de8b67c79412e
Kai Zhang <kaizen.agility@gmail.com>
2017-11-21 01:40:09 -0500

    Set up Router on React App.js. Moved Home components to container Home.

--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import TopNav from './components/TopNav'
-import MainNav from './components/MainNav'
-import Banner from './components/Banner'
-import AnimationHeader from './components/AnimationHeader'
+import TopNav from '../components/TopNav'
+import MainNav from '../components/MainNav'
+import Banner from '../components/Banner'
+import AnimationHeader from '../components/AnimationHeader'
 
 const Home = () => {
     return (
--- models/Student.js
+++ models/Student.js
@@ -18,10 +18,15 @@ Student.findbyUserName = userName => {
 Student.create = email => {
     return db.one(`
         INSERT INTO students
-        (first_name, last_name, phone, class, aboutme)
-        VALUES ($1, $2, $3, $4, $5, $6)
+        (first_name, last_name, phone, email, class, aboutme)
+        VALUES ($1, $2, $3, $4, $5, $6, $7)
         RETURNING *
+<<<<<<< HEAD
     `, [student.first_name, student.last_name, student.phone, student.class, student.aboutme]);
 };
 
 module.exports = Student;
+=======
+    `, [student.first_name, student.last_name, student.phone, student.email, student.class, student.aboutme]);
+};
+>>>>>>> Set up Router on React App.js. Moved Home components to container Home.
