cd20421ecb6f791eb134976aa771743af601166a
andrea lee <lee.ays.lee@gmail.com>
2017-11-21 12:13:26 -0500

    corrections to controller and models mainly around email

--- models/Student.js
+++ models/Student.js
@@ -7,7 +7,7 @@ Student.findAll = () => {
   }
 
 //should this be email and not id?
-Student.findbyUserName = (email) => {
+Student.findbyEmail = (email) => {
     return db.oneOrNone(`
         SELECT * FROM students
         WHERE email = $1
@@ -26,22 +26,18 @@ Student.create = (student) => {
     `, [student.email, student.first_name, student.last_name, student.phone, student.cycle, student.aboutme]);
 };
 
-Student.update = (student, id) => {
+Student.update = (student, email) => {
   return db.one(`
     UPDATE students SET
-    email = $1,
-    first_name = $2,
-    last_name = $3,
-    phone = $4,
-    cycle = $5,
-    aboutme = $6
-    WHERE id = $7
+    phone = $1,
+    aboutme = $2
+    WHERE email = $3
     RETURNING *
-  `,[student.email, student.first_name, student.last_name, student.phone, student.cycle, student.aboutme], id);
+  `,[student.phone, student.aboutme], email);
 };
 
-Student.destroy = (id) => {
-  return db.none('DELETE FROM students WHERE id = $1', id);
+Student.destroy = (email) => {
+  return db.none('DELETE FROM students WHERE id = $1', email);
 };
 
 module.exports = Student;
