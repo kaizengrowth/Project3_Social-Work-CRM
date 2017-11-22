acadf05b33b2cba3e09056ed366d35b1c4f7c9bf
bart369 <bartinpeguero@gmail.com>
2017-11-22 11:18:51 -0500

    set up routes in profilecontroller, moved methods in there

--- client/src/components/ProfileController.jsx
+++ client/src/components/ProfileController.jsx
@@ -11,18 +11,58 @@ class ProfileController extends React.Component {
      studentDataLoaded: false,
      selectedStudent: null,
    }
+   this.getStudentList = this.getStudentList.bind(this);
+   this.currentProfile = this.currentProfile.bind(this);
 
  }
 
+ componentDidMount(){
+   this.getStudentList();
+ }
+
+ getStudentList() {
+   //this will need to be adjusted depending on how we set up routes and server.js on backend
+    fetch('/api/students')
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          //the below will need to be adjusted to match however studentcontroller returns data
+          // but this is similar set up to how its usually been handled in class examples
+          studentList: res.data.students,
+          studentDataLoaded: true,
+        });
+      }).catch(err => console.log(err));
+  }
+
+  currentProfile(id){
+    //takes data from studentList and returns the student with the studid
+    // equal to the id argument. Got this from the wine homework, single page.
+     let profile = this.state.studentList.find(student =>{
+       return student.studid === id
+     })
+     this.setState({
+       selectedStudent: profile,
+     })
+
+    // or is it better to do another fetch like below?
+    // fetch(`api/students/{id}`)
+    //   .then(res => res.json())
+    //   .then(res => {
+    //     this.setState({
+    //       selectedStudent: res,
+    //     });
+    //   }).catch(err => console.log(err));
+  }
 
 
   render(){
     return(
+      <Router>
         <div>
-          <Dashboard />
-          <Route path="/profile" component={StudentProfileContainer} />
-          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/api/students/:id" component={StudentProfileContainer} />
+          <Route path="/api/students" render={() => (<Dashboard studentDataLoaded={this.state.studentDataLoaded} students={this.state.studentList} profile= {this.state.selectedStudent}/>)}/>
         </div>
+      </Router>
     )
   }
 
--- client/src/containers/Dashboard.jsx
+++ client/src/containers/Dashboard.jsx
@@ -11,47 +11,8 @@ class Dashboard extends React.Component {
      studentDataLoaded: false,
      selectedStudent: null,
    }
-   this.getStudentList = this.getStudentList.bind(this);
-   this.currentProfile = this.currentProfile.bind(this);
+
  }
-
- componentDidMount(){
-   this.getStudentList();
- }
-
- getStudentList() {
-   //this will need to be adjusted depending on how we set up routes and server.js on backend
-    fetch('/api/students')
-      .then(res => res.json())
-      .then(res => {
-        this.setState({
-          //the below will need to be adjusted to match however studentcontroller returns data
-          // but this is similar set up to how its usually been handled in class examples
-          studentList: res.data.students,
-          studentDataLoaded: true,
-        });
-      }).catch(err => console.log(err));
-  }
-
-  currentProfile(id){
-    //takes data from studentList and returns the student with the studid
-    // equal to the id argument. Got this from the wine homework, single page.
-     let profile = this.state.studentList.find(student =>{
-       return student.studid === id
-     })
-     this.setState({
-       selectedStudent: profile,
-     })
-
-    // or is it better to do another fetch like below?
-    // fetch(`api/students/{id}`)
-    //   .then(res => res.json())
-    //   .then(res => {
-    //     this.setState({
-    //       selectedStudent: res,
-    //     });
-    //   }).catch(err => console.log(err));
-  }
 
 
   render(){
