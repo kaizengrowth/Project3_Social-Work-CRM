8fc71f98824c4788f2cb2ed466ece9357ca8a911
Kai Zhang <kaizen.agility@gmail.com>
2017-11-22 22:15:06 -0500

    Added password field on student model, controller, route, registration form. Added login to TopNav.

--- controllers/students-controller.js
+++ controllers/students-controller.js
@@ -33,81 +33,66 @@ studentsController.show = (req, res, next) => {
 //added next...?  do I need to add email?
 
 studentsController.create = (req, res, next) => {
-
-    // const salt = bcrypt.genSaltSync();
-    // const hash = bcrypt.hashSync(req.body.password, salt);
-    Student.create({
-        email: req.body.email,
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        phone: req.body.phone,
-        cycle: req.body.cycle,
-        aboutme: req.body.aboutme,
-        ispriority: false,
-
-    }).then(student => {
-        // req.login(student, (err) => {
-        //     if (err) return next(err);
-        // res.redirect('/intake');
-        res.status(201).json({
-            message: 'student created successfully & this will redirect to intake form once it exists',
-            data: {
-                student: student,
-            }
-        });
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-};
-
-
-
-
-//     }).then(student => {
-//         req.login(student, (err) => {
-//             if (err) return next(err);
-//             res.redirect('/intake');
-//         });
-//     }).catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//     });
-// };
-
-studentsController.update = (req, res, next) => {
-    Student.update({
-            email: req.body.email,
-            first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            phone: req.body.phone,
-            cycle: req.body.cycle,
-            aboutme: req.body.aboutme,
-        }, req.params.email)
-        .then(student => {
-            res.status(202).json({
-                message: 'updated successfully',
-                data: {
-                    student: student,
-                },
+        const salt = bcrypt.genSaltSync();
+        const hash = bcrypt.hashSync(req.body.password, salt);
+        Student.create({
+                email: req.body.email,
+                password_digest: hash,
+                first_name: req.body.first_name,
+                last_name: req.body.last_name,
+                phone: req.body.phone,
+                cycle: req.body.cycle,
+                aboutme: req.body.aboutme,
+                ispriority: false,
             }).then(student => {
-                res.redirect('/profile')
-            });
-        }).catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-};
+                    req.login(student, (err) => {
+                        if (err) return next(err);
+                        res.redirect('/intake');
+                        res.status(201).json({
+                            message: 'student created successfully & this will redirect to intake form once it exists',
+                            data: {
+                                student: student,
+                            }
+                        });
+                    }).catch(err => {
+                        console.log(err);
+                        res.status(500).json(err);
+                    });
+                };
 
-studentsController.delete = (req, res, next) => {
-    Student.destroy(req.params.email)
-        .then(() => {
-            res.redirect('/index');
-        }).catch(err => {
-            res.status(500).json({
-                err,
-            });
-        });
-}
+                studentsController.update = (req, res, next) => {
+                    Student.update({
+                            email: req.body.email,
+                            first_name: req.body.first_name,
+                            last_name: req.body.last_name,
+                            phone: req.body.phone,
+                            cycle: req.body.cycle,
+                            aboutme: req.body.aboutme,
+                        }, req.params.email)
+                        .then(student => {
+                            res.status(202).json({
+                                message: 'updated successfully',
+                                data: {
+                                    student: student,
+                                },
+                            }).then(student => {
+                                res.redirect('/profile')
+                            });
+                        }).catch(err => {
+                            console.log(err);
+                            res.status(500).json(err);
+                        });
+                };
 
-module.exports = studentsController;
+                studentsController.delete = (req, res, next) => {
+                    Student.destroy(req.params.email)
+                        .then(() => {
+                            res.redirect('/index');
+                        }).catch(err => {
+                            res.status(500).json({
+                                err,
+                            });
+                        });
+                }
+
+                module.exports = studentsController;
