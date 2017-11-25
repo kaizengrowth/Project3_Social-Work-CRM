d1ed5b1ae248e5cc5b885fd6050708a85fa0992c
bart369 <bartinpeguero@gmail.com>
2017-11-25 16:05:41 -0500

    Styling studentprofile component

--- client/src/components/StudentProfile.jsx
+++ client/src/components/StudentProfile.jsx
@@ -4,12 +4,14 @@ const StudentProfile = (props) => {
   let student = props.studentInfo;
   return(
     <div className= 'student_profile'>
-      <img src={student.image} alt='' />
-      <h1>{student.first_name + ' ' + student.last_name}</h1>
-      <p>{'E-mail: ' + student.email + '   ' + 'Phone: ' + student.phone}</p>
-      <p>{student.aboutme}</p>
-      {/* ACTUALLY MAYBE THIS WILL ONLY SHOW UP ON THE SOCIAL WORKERS PAGE?? */}
-      <p>{student.studentnotes}</p>
+      <div className='student_info'>
+        <img src={student.image} alt='' />
+        <h1>{student.first_name + ' ' + student.last_name}</h1>
+        <p>{'E-mail: ' + student.email + '   ' + 'Phone: ' + student.phone}</p>
+        <p>{student.aboutme}</p>
+        {/* ACTUALLY MAYBE THIS WILL ONLY SHOW UP ON THE SOCIAL WORKERS PAGE?? */}
+        <p>{student.studentnotes}</p>
+      </div>
     </div>
   )
 }
