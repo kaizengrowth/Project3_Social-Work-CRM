16bc54b46800f172cce3e625906746d6d6a07d1a
bart369 <bartinpeguero@gmail.com>
2017-11-25 19:26:55 -0500

    Referrals styling

--- client/src/components/Referrals.jsx
+++ client/src/components/Referrals.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
 const Referrals = (props) => {
-
-return null
+  return(
+    <div className= 'referrals'>
+      <div className='referrals_header'>
+        <h2> Referrals Section </h2>
+      </div>
+    </div>
+  )
 
 }
 
