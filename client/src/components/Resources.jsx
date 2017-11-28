88d36b81efbae350e6c372bc91fb75e8a8a3885d
bart369 <bartinpeguero@gmail.com>
2017-11-28 11:15:28 -0500

    intake component switches to resources component

--- client/src/components/Resources.jsx
+++ client/src/components/Resources.jsx
@@ -1,9 +1,7 @@
+
 import React from 'react';
 import Intake from './Intake';
 import ResourceInfo from './ResourceInfo';
-
-// HAVE DASHBOARD AND STUDENTPROFILECONTAINER EACH SEND A PROP EQUAL TO EITHER STUDENT OR WORKER.
-// GIVE A STATE THAT PROP. WORKER GIVES ACCESS TO AN EDIT/ADD BUTTON, STUDENT DOES CAN JUST READ?
 
 class Resources extends React.Component {
   constructor(props){
@@ -29,18 +27,18 @@ class Resources extends React.Component {
       }).catch(err => console.log(err));
   }
 
+  //WILL GET AN ERROR MESSAGE IF THE STUDENT HAS FILLED THE INTAKE FORM MORE THAN ONCE.
+  //SO WILL ALWAYS GET THE SECOND OPTION OF THE TERNARY BELOW
+
   render(){
     return (
       <div className= 'resources'>
 
       {this.state.showResources ?
-
         (<div className= 'resources_header'>
           <ResourceInfo showResources = {this.state.showResources} />
         </div>)
-
         :
-
         (<div>
           <Intake  studentInfo = {this.props.studentInfo} showResources = {this.showResources} />
         </div>)}
